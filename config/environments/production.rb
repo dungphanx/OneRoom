6a6cd56f8492afe906804bb14c30178b3c3755ff
Dung Phan <dungphanx@gmail.com>
2016-07-28 00:28:18 +0700

    Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = [
+  'https://thawing-woodland-83108.herokuapp.com', 
+  'https://thawing-woodland-83108.herokuapp.com'
+]
 end
